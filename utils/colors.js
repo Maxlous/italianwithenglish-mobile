1d32333e9bbe7d379daed91597af138bd7e1e180
Burak Gündüz <burakkgunduz21@gmail.com>
2021-08-08 20:28:28 +0300

    colors.js added

---
+++ utils/colors.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  bg: '#fff',
+  fontColor: '#282c36',
+  navLink: '#fef9ef',
+  headerBackground: '#282c36',
+};
+
+export const darkTheme = {
+  bg: '#282c36',
+  fontColor: '#fef9ef',
+  navLink: '#282c36',
+  headerBackground: '#fff',
+};
+
+export const primaryColors = {
+  caribbeanGreen: '#17c390',
+  floralWhite: '#fef9ef',
+  redSalsa: '#fe4d53',
+};
